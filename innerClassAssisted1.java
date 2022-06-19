bba8581f44f0bf88594f7ae84a3c3c38710b6e24
sreenivasbt <107801404+sreenivasbt@users.noreply.github.com>
2022-06-19 22:16:36 +0530

    Create innerClassAssisted1.java
    Created inner class assisted file

---
+++ innerClassAssisted1.java
@@ -0,0 +1,27 @@
+package fs.corejava.inner.cls;
+
+public class innerClassAssisted1 {
+
+    private String msg="Welcome to Java"; 
+    
+    class Inner{  
+     void hello(){System.out.println(msg+", Let us start learning Inner Classes");}  
+    }  
+
+
+   public static void main(String[] args) {
+
+       innerClassAssisted1 obj=new innerClassAssisted1();
+       innerClassAssisted1.Inner in=obj.new Inner();  
+       in.hello();  
+   }
+
+}
+
+//anonymous inner class
+abstract class AnonymousInnerClass {
+       public abstract void display();
+    }
+
+
+
